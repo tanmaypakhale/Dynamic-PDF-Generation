2323f77ca1edae54ab4fbd53156967f6c8e8715c
tanmaypakhale <tanmaypakhale@gmail.com>
2023-10-02 00:35:08 +0530

    file added

--- server.js
+++ server.js
@@ -11,14 +11,17 @@ app.listen(4000, () => {
 
 async function webPageToPDF(){
     const browser = await puppeteer.launch({
-      headless: "new",
+      headless: 'new',
       args: [
         "--disable-setuid-sandbox",
         "--no-sandbox",
         "--single-process",
         "--no-zygote",
       ],
-      executablePath: "/usr/bin/google-chrome-stable",
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
     const page = await browser.newPage();
 
